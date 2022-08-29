c732cafa91001939297fb707154c1f7beac4859f
JoasCamargo <jcravocamargo74@gmail.com>
2022-08-29 20:39:01 -0300

    conectando com o banco de dados

---
+++ DevagramCSharp/Models/DevagramContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevagramCSharp.Models
+{
+    public class DevagramContext : DbContext
+    {
+        public DevagramContext(DbContextOptions<DevagramContext> option) : base(option)
+        {
+        }
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
