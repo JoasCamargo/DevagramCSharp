cf177a5bbc2ca26123c6a867568ca760d878d6f1
JoasCamargo <jcravocamargo74@gmail.com>
2022-08-29 16:44:48 -0300

    Adicionado UsuarioController

---
+++ DevagramCSharp/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevagramCSharp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : BaseController
+    {
+        
+        public readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult ObterUsuario()
+        {
+            try
+            {
+                Usuario usuario = new Usuario()
+                {
+                    Email = "joas@devaria.com.br",
+                    Nome = "Joás Camargo",
+                    Id = 100
+                };
+
+                return Ok(usuario);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Ocorreu um erro ao obter o usuário");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorRepostaDto()
+                {
+                    Descricao = "Ocorreu o seguinte erro: " + e.Message,
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            }
+           
+        }
+    }
+}
