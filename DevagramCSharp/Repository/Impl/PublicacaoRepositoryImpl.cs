80747f628c92b288ef887e4cd9beb87003039477
JoasCamargo <jcravocamargo74@gmail.com>
2022-09-07 11:22:45 -0300

    Implementação do GET FeedUsuario

--- DevagramCSharp/Repository/Impl/PublicacaoRepositoryImpl.cs
+++ DevagramCSharp/Repository/Impl/PublicacaoRepositoryImpl.cs
@@ -29,6 +29,21 @@ namespace DevagramCSharp.Repository.Impl
             return feed.ToList();
         }
 
+        public List<PublicacaoFeedRespostaDto> GetPublicacoesFeedUsuario(int idUsuario)
+        {
+            var feedusuario =
+                from publicacoes in _context.Publicacoes
+                where publicacoes.IdUsuario == idUsuario
+                select new PublicacaoFeedRespostaDto
+                {
+                    IdPublicacao = publicacoes.Id,
+                    Descricao = publicacoes.Descricao,
+                    Foto = publicacoes.Foto,
+                    IdUsuario = publicacoes.IdUsuario
+                };
+            return feedusuario.ToList();
+        }
+
         public void Publicar(Publicacao publicacao)
         {
             _context.Add(publicacao);
